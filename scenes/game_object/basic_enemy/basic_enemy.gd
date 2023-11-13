b340cb55dd5d16c6e25a4f66231071203116684a
MinSungJe <minje813@naver.com>
2023-11-13 18:41:10 +0900

    Implementing Damage

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -4,8 +4,6 @@ const MAX_SPEED = 40
 
 @onready var health_component = $HealthComponent as HealthComponent
 
-func _ready():
-	$Area2D.area_entered.connect(on_area_entered)
 
 
 func _process(delta):
@@ -18,8 +16,3 @@ func get_direction_to_player():
 	var player_node = get_tree().get_first_node_in_group("player") as Node2D
 	if player_node: return (player_node.global_position - global_position).normalized()
 	return Vector2.ZERO
-
-
-func on_area_entered(other_area : Area2D):
-	health_component.damage(100)
-	
