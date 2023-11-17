a463aa83bca79c28ca56857aec9bf079c5e43a9f
MinSungJe <minje813@naver.com>
2023-11-17 22:51:10 +0900

    Upgrade Select 기능 추가

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -1,9 +1,20 @@
 extends PanelContainer
 
+signal selected
+
 @onready var name_label: Label = %NameLabel
 @onready var discription_label: Label = %DiscriptionLabel
+
+
+func _ready():
+	gui_input.connect(on_gui_input)
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	discription_label.text = upgrade.discription
+
+
+func on_gui_input(event: InputEvent):
+	if event.is_action_pressed("left_click"):
+		selected.emit()
