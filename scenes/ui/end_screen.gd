f19308c30fb9b2c0f37ab23cae64e3def3971d29
MinSungJe <minje813@naver.com>
2023-11-22 18:09:06 +0900

    Defeat Screen 추가

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -5,7 +5,12 @@ func _ready():
 	get_tree().paused = true
 	%RestartButton.pressed.connect(on_restart_button_pressed)
 	%QuitButton.pressed.connect(on_quit_button_pressed)
-	
+
+
+func set_defeat():
+	%TitleLabel.text = "Defeat"
+	%DiscriptionLabel.text = "You Lost!"
+
 
 func on_restart_button_pressed():
 	get_tree().paused = false
