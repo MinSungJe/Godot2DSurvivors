2f47c28454975200471f2496533d5f917085b53f
MinSungJe <minje813@naver.com>
2023-12-29 21:55:06 +0900

    SFX 추가

--- scenes/component/hurtbox_component.gd
+++ scenes/component/hurtbox_component.gd
@@ -1,6 +1,8 @@
 extends Area2D
 class_name HurtboxComponent
 
+signal hit
+
 @export var health_component: HealthComponent
 
 var floating_text_scene = preload("res://scenes/ui/floating_text.tscn")
@@ -26,3 +28,5 @@ func on_area_entered(other_area: Area2D):
 	var format_string = "%0.1f"
 	if round(hitbox_component.damage) == hitbox_component.damage: format_string = "%0.0f"
 	floating_text.start(format_string % hitbox_component.damage)
+
+	hit.emit()
